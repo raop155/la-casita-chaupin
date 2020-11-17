17b4b7a6b45f69545f55e4594407c6eb27cd9616
Ricardo Olarte <raop155@hotmail.com>
2020-11-16 19:44:06 -0500

    Fix warnings messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Menu from './pages/Menu';
 
 function App() {
   return (
@@ -8,12 +7,10 @@ function App() {
       <header id='header'></header>
       <Router>
         <Switch>
-          <Router path='/' exact>
+          <Route path='/' exact>
             <Home />
-          </Router>
-          <Router path='/menu' exact>
-            {/* <Menu /> */}
-          </Router>
+          </Route>
+          <Route path='/menu' exact></Route>
         </Switch>
       </Router>
       <footer id='footer'></footer>
